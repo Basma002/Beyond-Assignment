e97095c4ec916c70f5538b22d68e899cc3fdc693
Basma002 <basmah@beyondcompany.sa>
2025-01-06 09:34:06 +0300

    Feat: Add assignment 9

---
+++ movie-web/src/app/layout.js
@@ -0,0 +1,32 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        </Providers>
+      </body>
+      <footer className="bg-gray-900 text-gray-400 p-4 mt-8 text-center">
+  <p>© {new Date().getFullYear()} IMDb Clone. All rights reserved.</p>
+</footer>
+
+    </html>
+  );
+}
