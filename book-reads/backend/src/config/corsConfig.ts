12e0e4e0ccbc1bad8d77e772a7ed7e5f3d9d115e
Basma002 <basmah@beyondcompany.sa>
2025-02-06 13:15:42 +0300

    refactor cors config origin

--- book-reads/backend/src/config/corsConfig.ts
+++ book-reads/backend/src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-  origin: ["https://frontend-cfansw7m2-basmas-projects-c8197bc0.vercel.app"],
+  origin: ["https://frontend-five-omega-89.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
