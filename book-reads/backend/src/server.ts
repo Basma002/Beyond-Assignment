16e62eba7ee9316f5d6b2c17ffc8d5b6df7ffec3
Basma002 <basmah@beyondcompany.sa>
2025-02-08 14:39:47 +0300

    refactor package.json

--- book-reads/backend/src/server.ts
+++ book-reads/backend/src/server.ts
@@ -1,9 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app";
-import { Request, Response } from "express";
 
 dotenv.config();
 
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
+// Vercel expects an exported default function
+export default app;
