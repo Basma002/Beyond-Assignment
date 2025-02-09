04fa50132d1fc1c5fff6805cf8bbf8e6e2ce8557
Basma002 <basmah@beyondcompany.sa>
2025-02-09 10:10:19 +0300

    refactoor cors

--- book-reads/backend/src/app.ts
+++ book-reads/backend/src/app.ts
@@ -14,8 +14,8 @@ const app: Express = express();
 app.use(corsConfig); 
 app.use(express.json()); 
 
-app.use((req, res, next) => {   
-  res.setHeader("Access-Control-Allow-Origin", "frontend-five-omega-89.vercel.app");
+app.use((req, res, next) => {        
+  res.setHeader("Access-Control-Allow-Origin", "https://frontend-gr47fajr3-basmas-projects-c8197bc0.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
